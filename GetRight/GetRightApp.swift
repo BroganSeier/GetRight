a72fd1e0d644b3ede10a0f422e8c7c2686d047f9
Brogan Seier <broganseier21@gmail.com>
2024-08-23 18:06:32 -0500

    Initial Commit

---
+++ GetRight/GetRightApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetRightApp.swift
+//  GetRight
+//
+//  Created by brogan seier on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GetRightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
